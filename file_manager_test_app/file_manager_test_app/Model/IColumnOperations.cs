e7fd60f33bd9bcc3c2dd0c39e0edd1b78e5f89de
Yura95st <yura95st@gmail.com>
2013-10-21 18:09:57 +0300

    interface of Column class which shows what operations are allowed to do with Column class objects

---
+++ file_manager_test_app/file_manager_test_app/Model/IColumnOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace file_manager_test_app.Model
+{
+    interface IColumnOperations
+    {
+        void Select(int[] elements);
+
+        void DeleteSelectedElements();
+
+        void CopySelectedElementsTo(string destination);
+
+        void MoveSelectedElementsTo(string destination);
+
+        void CreateNewDirectory(string name);
+
+        void CreateNewTxtFile(string name);
+
+        void ClearSelection();
+
+        void Rename(string newName);
+    }
+}
