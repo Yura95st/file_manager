5e7b124f8218278967a907365a48fa36cdcf2c38
Yura95st <yura95st@gmail.com>
2013-10-21 18:08:20 +0300

    added special classes MyDriveInfo and MyFileSystemInfo to provide necessary info to View and nothing more (no methods, only info)

---
+++ file_manager_test_app/file_manager_test_app/Model/MyDriveInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+namespace file_manager_test_app.Model
+{
+    class MyDriveInfo
+    {
+        private long _availableFreeSpace;
+        private string _driveFormat;
+        private DriveType _driveType;
+        private string _name;
+        private long _totalFreeSpace;
+        private long _totalSize;
+        private string _volumeLabel;
+
+        public long AvailableFreeSpace
+        {
+            get
+            {
+                return _availableFreeSpace;
+            }
+        }
+
+        public string DriveFormat
+        {
+            get
+            {
+                return _driveFormat;
+            }
+        }
+
+        public string DriveType
+        {
+            get
+            {
+                return _driveType;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public long TotalFreeSpace
+        {
+            get
+            {
+                return _totalFreeSpace;
+            }
+        }
+
+        public long TotalSize
+        {
+            get
+            {
+                return _totalSize;
+            }
+        }
+
+        public string VolumeLabel
+        {
+            get
+            {
+                return _volumeLabel;
+            }
+        }
+
+        public MyDriveInfo(long availableFreeSpace, string driveFormat, DriveType driveType,
+            string name, long totalFreeSpace, long totalSize, string volumeLabel)
+        {
+            _availableFreeSpace = availableFreeSpace;
+            _driveFormat = driveFormat;
+            _driveType = driveType;
+            _name = name;
+            _totalFreeSpace = totalFreeSpace;
+            _totalSize = totalSize;
+            _volumeLabel = volumeLabel;
+        }
+    }
+}
---
+++ file_manager_test_app/file_manager_test_app/Model/MyFileSystemInfo.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_manager_test_app.Model
+{
+    public class MyFileSystemInfo
+    {
+        private string _name;
+        private long _size;
+        private string _extension;
+        private string _fullName;
+        private DateTime _creationTime;
+        private DateTime _creationTimeUtc;
+        private DateTime _lastAccessTime;
+        private DateTime _lastAccessTimeUtc;
+        private DateTime _lastWriteTime;
+        private DateTime _lastWriteTimeUtc;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public long Size
+        {
+            get
+            {
+                return _size;
+            }
+            set 
+            {
+                _size = value;
+            }
+        }
+
+        public string Extension
+        {
+            get
+            {
+                return _extension;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return _fullName;
+            }
+        }
+
+        public DateTime CreationTime
+        {
+            get
+            {
+                return _creationTime;
+            }
+        }
+
+        public DateTime CreationTimeUtc
+        {
+            get
+            {
+                return _creationTimeUtc;
+            }
+        }
+
+        public DateTime LastAccessTime
+        {
+            get
+            {
+                return _lastAccessTime;
+            }
+        }
+
+        public DateTime LastAccessTimeUtc
+        {
+            get
+            {
+                return _lastAccessTimeUtc;
+            }
+        }
+
+        public DateTime LastWriteTime
+        {
+            get
+            {
+                return _lastWriteTime;
+            }
+        }
+
+        public DateTime LastWriteTimeUtc
+        {
+            get
+            {
+                return _lastWriteTimeUtc;
+            }
+        }
+
+        public MyFileSystemInfo(string name, string extension, string fullName, 
+            DateTime creationTime, DateTime creationTimeUtc, DateTime lastAccessTime,
+            DateTime lastAccessTimeUtc, DateTime lastWriteTime, DateTime lastWriteTimeUtc)
+        {
+            _name = name;
+            _size = 0;
+            _extension = extension;
+            _fullName = fullName;
+            _creationTime = creationTime;
+            _creationTimeUtc = creationTimeUtc;
+            _lastAccessTime = lastAccessTime;
+            _lastAccessTimeUtc = lastAccessTimeUtc;
+            _lastWriteTime = lastWriteTime;
+            _lastWriteTimeUtc = lastWriteTimeUtc;
+        }
+    }
+}
