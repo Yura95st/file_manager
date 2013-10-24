0ed05ac621d683c567b600255baa76d25ba57fc7
Yura95st <yura95st@gmail.com>
2013-10-24 23:45:15 +0300

    We have 2 items of ColumnController, DriveView and FileSystemElementView classes for each part of file manager. DriveView and FileSystemElementView are able to interact with controller, and controller can interract with them too.

---
+++ file_manager_test_app/file_manager_test_app/Controllers/ColumnController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace file_manager_test_app.Controllers
+{
+    //Based on "mediator" pattern
+    public class ColumnController
+    {
+        private Model.Column _column;
+        private Views.DriveView _driveView;
+        private Views.FileSystemElementView _fsElementView;
+
+        public ColumnController()
+        {
+            _column = new Model.Column();
+        }
+
+        public Views.DriveView DriveView
+        {
+            set
+            {
+                _driveView = value;
+            }
+        }
+
+        public Views.FileSystemElementView FsElementView
+        {
+            set
+            {
+                _fsElementView = value;
+            }
+        }
+
+        public void Select(int[] elements)
+        {
+            if (elements.Length > 0)
+            {
+                _column.Select(elements);
+            }
+        }
+
+        public void CopyTo(string destination)
+        {
+            _column.CopySelectedElementsTo(destination);
+        }
+
+        public void SetActiveElement(int id)
+        {
+            _column.ActiveElement = id;
+        }
+
+        public void Delete()
+        {
+            _column.DeleteSelectedElements();
+        }
+
+        public void MoveTo(string destination)
+        {
+            _column.MoveSelectedElementsTo(destination);
+        }
+
+        public void Read()
+        {
+            _column.Read();
+            _fsElementView.Refresh();
+        }
+
+        public void CreateNewTxtFile(string name)
+        {
+            _column.CreateNewTxtFile(name);
+        }
+
+        public void CreateNewDirectory(string name)
+        {
+            _column.CreateNewDirectory(name);
+        }
+
+        public void Merge()
+        {
+            
+        }
+
+        public void Rename(string newName)
+        {
+            _column.Rename(newName);
+        }
+
+        public void Search(string query)
+        {
+            
+        }
+
+        public void SpecialMegre()
+        {
+            
+        }
+
+        public void ClearSelection()
+        {
+            _column.ClearSelection();
+        }
+
+        public void Edit()
+        {
+           
+        }
+
+        public List<Model.MyFileSystemInfo> GetElementsList()
+        {
+            return _column.GetElementsList();
+        }
+
+        public List<Model.MyDriveInfo> GetDrivesList()
+        {
+            return _column.GetDrivesList();
+        }
+
+        public void SetActiveDrive(int id)
+        {
+            _column.ActiveDrive = id;
+            _fsElementView.Refresh();
+        }
+
+        public void SetCurrentPath(string path)
+        {
+            _column.CurrentPath = path;
+            _fsElementView.Refresh();
+        }
+
+        public bool isCurrentPathRoot()
+        {
+            string root = _column.GetDrivesList()[_column.ActiveDrive].RootDirectory;
+            string path = _column.CurrentPath;
+            return root == path;
+        }
+
+        public void SortByDate()
+        {
+       
+        }
+
+        public void SortByExtention()
+        {
+        
+        }
+
+        public void SortByName()
+        {
+
+        }
+
+        public void SortBySize()
+        {
+         
+        }
+
+    }
+}
+
--- file_manager_test_app/file_manager_test_app/Views/MainView.cs
+++ file_manager_test_app/file_manager_test_app/Views/MainView.cs
@@ -1,40 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace file_manager_test_app.Views
 {
     public class MainView
     {
         private Window _mainWindow;
-        private DriveView _leftDriveView;
-        private DriveView _rightDriveView;
-        private FileSystemElementView _leftFileSystemElementView;
-        private FileSystemElementView _rightFileSystemElementView;
+        private List<DriveView> _driveViewList;
+        private List<FileSystemElementView> _fileSystemElementViewList;
+        private List<Controllers.ColumnController> _controllerList;
 
         public MainView(Window mainWindow)
         {
             _mainWindow = mainWindow;
-            _leftDriveView = new DriveView(0, _mainWindow);
-            _rightDriveView = new DriveView(1, _mainWindow);
+            _driveViewList = new List<DriveView>();
+            _fileSystemElementViewList = new List<FileSystemElementView>();
+            _controllerList = new List<Controllers.ColumnController>();
 
-            _leftFileSystemElementView = new FileSystemElementView(0, _mainWindow);
-            _rightFileSystemElementView = new FileSystemElementView(1, _mainWindow);
+            for (int i = 0; i < 2; i++)
+            {
+                Controllers.ColumnController controller = new Controllers.ColumnController();
+                FileSystemElementView fsElementView = new FileSystemElementView(i, _mainWindow, controller);
+                DriveView driveView = new DriveView(i, _mainWindow, controller);
+
+                controller.DriveView = driveView;
+                controller.FsElementView = fsElementView;
+
+                _controllerList.Add(controller);
+                _fileSystemElementViewList.Add(fsElementView);
+                _driveViewList.Add(driveView);
+            }
         }
 
         public void Init()
         {
-            _leftDriveView.BuildDriveButtonsPanel();
-            _rightDriveView.BuildDriveButtonsPanel();
+            for (int i = 0; i < 2; i++)
+            {
+                DriveView dv = _driveViewList[i];
+                dv.BuildDriveButtonsPanel();
+                dv.BuildDriveComboBoxPanel();
+                dv.SetDriveInfoLabel();
+                dv.SetActiveDrive(0);
 
-            _leftDriveView.BuildDriveComboBoxPanel();
-            _rightDriveView.BuildDriveComboBoxPanel();
-
-            _leftDriveView.BuildDriveInfoLabel();
-            _rightDriveView.BuildDriveInfoLabel();
-
-            _leftFileSystemElementView.BuildList();
-            _rightFileSystemElementView.BuildList();
+                FileSystemElementView fv = _fileSystemElementViewList[i];
+                fv.SetElementsList();
+            }
         }
     }
 }
