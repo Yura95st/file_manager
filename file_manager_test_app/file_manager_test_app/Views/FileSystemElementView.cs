e5bcde59134a574f7fc260bdf96ad16e8dece8f1
Yura95st <yura95st@gmail.com>
2013-11-04 14:59:22 +0200

    added ability to select elements in ListView, added hot-keys

--- file_manager_test_app/file_manager_test_app/Views/FileSystemElementView.cs
+++ file_manager_test_app/file_manager_test_app/Views/FileSystemElementView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace file_manager_test_app.Views
 {
@@ -13,7 +14,10 @@ namespace file_manager_test_app.Views
         private Controllers.ColumnController _controller;
         private List<Model.MyFileSystemInfo> _elements;
 
-        private string[] _elementListBoxesNames = { "LeftElementListBox", "RightElementListBox" };
+        private string[] _elementListViewsNames = { "LeftElementList", "RightElementList" };
+        private string[] _listBoxPathNames = { "LeftListBoxPath", "RightListBoxPath" };
+        private string _textBoxPathLabelName = "TextBoxPathLabel";
+        private string _textBoxPathName = "TextBoxPath";
 
         public FileSystemElementView(int type, Window mainWindow, Controllers.ColumnController controller)
         {
@@ -28,53 +32,176 @@ namespace file_manager_test_app.Views
             _elements.Clear();
             _elements = _controller.GetElementsList();
             SetElementsList();
+            SetListBoxPath();
+            SetTextBoxLabelPath();
         }
 
         public void SetElementsList()
         {
-            ListBox listBox = (ListBox)_mainWindow.FindName(_elementListBoxesNames[_type]);
-            listBox.Items.Clear();
+            ListView listView = (ListView)_mainWindow.FindName(_elementListViewsNames[_type]);
+            if (listView.Items.Count == 0)
+            {
+                listView.SelectionChanged += SelectListViewElements;
+            }
+            listView.Items.Clear();            
+           
+            ListViewItem item = new ListViewItem();
 
+            if (!_controller.isCurrentPathRoot())
+            {
+                item.Content = "[..]";
+                item.Uid = "-1";
+
+                item.MouseDoubleClick += FileSystemElement_DoubleClick;
+                item.KeyDown += FileSystemElement_KeyDown;
+
+                listView.Items.Add(item);
+            }
+
+            List<Model.MyFileSystemInfo> elementList = new List<Model.MyFileSystemInfo>();
             int i = 0;
+
             foreach (Model.MyFileSystemInfo element in _elements)
             {
-                ListBoxItem item = new ListBoxItem();
+                item = new ListViewItem();
 
-                if (!_controller.isCurrentPathRoot() && i == 0)
-                {
-                    item.Content = "[..]";
-                    item.Uid = "-1";
-                }
-                else
-                {
-                    item.Content = "[" + element.Name + "]";
-                    item.Uid = "" + i;
-                }
-                item.MouseDoubleClick += fileSystemElement_Click;
-                item.KeyDown += fileSystemElement_KeyDown;
+                item.Content = "[" + element.Name + "]";
+                item.Uid = "" + i++;
 
-                listBox.Items.Add(item);
-                i++;
+                item.MouseDoubleClick += FileSystemElement_DoubleClick;
+                item.KeyDown += FileSystemElement_KeyDown;
+
+                listView.Items.Add(item);
             }
+
+            listView.SelectedIndex = 0;
         }
 
-        public void SetActiveElement()
-        { }
+        public void SetListBoxPath()
+        {
+            TextBox textBox = (TextBox)_mainWindow.FindName(_listBoxPathNames[_type]);
+            textBox.Text = _controller.GetCurrentPath().ToLower();
+        }
 
-        public void SelectElements()
-        { }
+        public void SetTextBoxLabelPath()
+        {
+            Label label = (Label)_mainWindow.FindName(_textBoxPathLabelName);
+            label.Content = _controller.GetCurrentPath().ToLower() + ">";
+        }
 
-        private void fileSystemElement_Click(object sender, RoutedEventArgs e)
+        public void SetAlertMessage(string msg)
+        {
+            MessageBoxResult result = MessageBox.Show(_mainWindow, msg, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.OK)
+            {
+                // Yes code here
+            }
+            else
+            {
+                // No code here
+            } 
+        }
+
+        private void SelectListViewElements(object sender, RoutedEventArgs e)
+        {
+            ListView lv = sender as ListView;
+            List<int> idList = new List<int>();
+
+            foreach (ListViewItem element in lv.SelectedItems)
+            {
+                idList.Add(Convert.ToInt32(element.Uid));
+            }
+
+            _controller.SelectElements(idList);
+
+            //active element is the last element in selection
+            int count = lv.SelectedItems.Count;
+            int lastSelectedElementId = 0;
+
+            if (count > 0)
+            {
+                ListViewItem lastSelectedElement = (ListViewItem)lv.SelectedItem;
+                lastSelectedElementId = Convert.ToInt32(lastSelectedElement.Uid);
+            }
+            _controller.SetActiveElement(lastSelectedElementId);
+        }
+
+        private void FileSystemElementRead(object sender)
         {
             ListBoxItem item = sender as ListBoxItem;
             int elementId = Convert.ToInt32(item.Uid);
 
-            _controller.SetActiveElement(elementId);
-            _controller.Read();
+            if (elementId == -1)
+            {
+                _controller.NavigationHistoryGoBack();
+            }
+            else
+            {
+                _controller.Read();
+            }
         }
 
-        private void fileSystemElement_KeyDown(object sender, RoutedEventArgs e)
+        private void ElementCopy()
         {
+            _controller.WriteToBuffer();
+        }
+
+        private void ElementCut()
+        {
+            //_controller.WriteToBuffer();
+        }
+
+        private void ElementPaste()
+        {
+            _controller.CopyTo(1); // 1 - means buffered copying
+        }
+
+        private void ElementCopyTo()
+        {
+            _controller.CopyTo();
+        }
+
+        private void ElementMoveTo()
+        {
+            _controller.MoveTo();
+        }
+
+        private void ElementDelete()
+        {
+            _controller.Delete();
+        }
+
+        private void FileSystemElement_DoubleClick(object sender, RoutedEventArgs e)
+        {
+            FileSystemElementRead(sender);
+        }
+
+        private void FileSystemElement_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                FileSystemElementRead(sender);
+            }
+            else if (e.Key == Key.C && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                ElementCopy();
+            }
+            else if (e.Key == Key.V && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                ElementPaste();
+            }
+            else if (e.Key == Key.F5)
+            {
+                ElementCopyTo();
+            }
+            else if (e.Key == Key.F6)
+            {
+                ElementMoveTo();
+            }
+            else if (e.Key == Key.F8)
+            {
+                ElementDelete();
+            }
         }
     }
 }
