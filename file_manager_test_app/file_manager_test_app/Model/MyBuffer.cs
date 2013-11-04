92b2ed7d7422c4be3d69f91077452ac133b0f6d0
Yura95st <yura95st@gmail.com>
2013-11-04 14:36:58 +0200

    added MyBuffer class for ability to copy with the hot-keys Ctrl+C - into the buffer

---
+++ file_manager_test_app/file_manager_test_app/Model/MyBuffer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace file_manager_test_app.Model
+{
+    class MyBuffer
+    {                
+        private static MyBuffer _singletonInstance = null;
+        private List<FileSystemInfo> _elements = new List<FileSystemInfo>();
+
+        private MyBuffer() 
+        { }
+
+        public static MyBuffer GetInstance()
+        {
+            if (_singletonInstance == null)
+            {
+                _singletonInstance = new MyBuffer();
+            }
+            return _singletonInstance;
+        }
+
+        public List<FileSystemInfo> GetElements()
+        {
+            return _elements;
+        }
+
+        public void AddElement(FileSystemInfo element)
+        {
+            _elements.Add(element);
+        }
+
+        public void Clear()
+        {
+            _elements.Clear();
+        }
+    }
+}
